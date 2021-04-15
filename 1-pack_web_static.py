78e09cf22a8fedd44f1b02f17747a1ee888df2fa
nehpets <nehpets0701@gmail.com>
2021-04-15 20:39:31 +0000

    0/1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """first fabric"""
-=======
 """ 1-pack_web_static.py """
->>>>>>> 60cd01154419f0f649e09cb0453f0e887492c965
 from datetime import datetime
 from fabric.api import local
 
 
 def do_pack():
-<<<<<<< HEAD
     """pack function"""
     time = datetime.now().strftime("%Y%m%d%H%M%S")
     local("mkdir -p versions")
@@ -19,18 +15,3 @@ def do_pack():
     except:
         return None
     return "versions/web_static_{}.tgz web_static".format(time)
-=======
-    """
-    Description:
-    A Fabric function that generates a .tgz archive
-    from the contents of the web_static folder
-    """
-    time_name = datetime.now().strftime("%Y%m%d%H%M%S")
-    local("mkdir -p versions")
-    try:
-        local("tar -cvzf versions/web_static_{}.tgz web_static"
-              .format(time_name))
-    except:
-        return None
-    return "versions/web_static_{}.tgz web_static".format(time_name)
->>>>>>> 60cd01154419f0f649e09cb0453f0e887492c965
