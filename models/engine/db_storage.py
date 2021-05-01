04ebf3c6c272ac530b1f769bc53ed2c2aebe4a96
Stephen <nehpets0701@gmail.com>
2021-05-01 15:04:54 -0400

    7

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -66,3 +66,7 @@ class DBStorage:
                                        expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+
+    def close(self):
+        """close"""
+        self.__session.close()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -61,3 +61,7 @@ class FileStorage:
         if obj is not None:
             del FileStorage.__objects[obj.to_dict()['__class__'] + '.' +
                                       obj.id]
+
+    def close(self):
+        """deserializes json"""
+        self.reload()
